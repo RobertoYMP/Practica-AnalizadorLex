89bb2929c191d58646dcc37bccfb452c8e05d8fa
yahir pedraza <yahirpedraza-26@hotmail.com>
2023-10-08 00:00:26 -0600

    Interprete y lectura de archivo

--- src/main/java/com/mycompany/analizadorlex/AnalizadorLEX.java
+++ src/main/java/com/mycompany/analizadorlex/AnalizadorLEX.java
@@ -2,15 +2,78 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
+
 package com.mycompany.analizadorlex;
 
-/**
- *
- * @author user
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+
 public class AnalizadorLEX {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    static boolean existenErrores = false;
+
+    public static void main(String[] args) throws IOException {
+        String archivoPath = "archivo.txt"; // Ruta relativa al archivo en la misma carpeta que el proyecto
+        
+        if(args.length > 1) {
+            System.out.println("Uso correcto: interprete [archivo.txt]");
+            System.exit(64);
+        } else if(args.length == 1){
+            archivoPath = args[0]; // Usar el archivo proporcionado como argumento
+        }
+
+        ejecutarArchivo(archivoPath);
+    }
+
+    private static void ejecutarArchivo(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        ejecutar(new String(bytes, Charset.defaultCharset()));
+
+        // Se indica que existe un error
+        if(existenErrores) System.exit(65);
+    }
+
+    private static void ejecutarPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for(;;){
+            System.out.print(">>> ");
+            String linea = reader.readLine();
+            if(linea == null) break; // Presionar Ctrl + D
+            ejecutar(linea);
+            existenErrores = false;
+        }
+    }
+
+    private static void ejecutar(String source) {
+        try{
+            Scanner scanner = new Scanner(source);
+            List<Token> tokens = scanner.scan();
+
+            for(Token token : tokens){
+                System.out.println(token);
+            }
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    static void error(int linea, String mensaje){
+        reportar(linea, "", mensaje);
+    }
+
+    private static void reportar(int linea, String posicion, String mensaje){
+        System.err.println(
+                "[linea " + linea + "] Error " + posicion + ": " + mensaje
+        );
+        existenErrores = true;
     }
 }
